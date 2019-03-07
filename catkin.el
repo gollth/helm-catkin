9fefed7ece4fd46d88c0617a240ec8d91a8fbc9f
goll_th <thore.goll@franka.de>
2019-03-07 10:59:41 +0100

    refactor to catkin-cd

--- catkin.el
+++ catkin.el
@@ -7,23 +7,33 @@
 
 ;;; Code:
 
-(defun catkin-source-command (ws command)
-  "Prepends a source `ws'/devel/setup.bash before `command', if such a file exists"
+(defun catkin-cd (ws cmd)
+  (format "cd %s && %s" ws cmd))
+
+(defun catkin-init (ws)
+  "(Re-)Initializes a catkin workspace at path `ws'"
+  (unless (file-exists-p ws)
+    (unless (y-or-n-p (format "Path %s does not exist. Create?" ws))
+      (error "Cannot initialize workspace `%s' since it doesn't exist" ws))
+    (make-directory ws)
+    (make-directory (format "%s/src" ws)))
+  (call-process-shell-command (catkin-cd ws "catkin init")))
+
+(defun catkin-source (ws command)
+  "Prepends a source `ws'/devel/setup.bash before `command'"
   (let ((setup-file (format "%s/devel/setup.bash" ws)))
     (if (file-exists-p setup-file)
         (format "source %s && %s" setup-file command)
-      command
-      )))
+      command)
+     ))
 
 (defun catkin-print-config (ws)
   "Prints the catkin config of `ws' to a new buffer called *catkin-config*"
   (switch-to-buffer-other-window "*catkin-config*")
   (erase-buffer)
-  (shell-cd ws)
-  (catkin-source-setup ws)
-  (call-process-shell-command (catkin-source-command ws "catkin config") nil t)
-  (other-window))
+  (call-process-shell-command (catkin-source ws (catkin-cd ws "catkin config")) nil t)
+  (other-window 1))
 
-(catkin-print-config "~/ros/util")
+(catkin-print-config "~/hello")
 
 ;;; catkin.el ends here

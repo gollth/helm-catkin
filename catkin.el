1e0966e719471791619dedcee57cc6a47d04ba1b
goll_th <thore.goll@franka.de>
2019-03-07 10:03:05 +0100

    first test with showing configs

--- catkin.el
+++ catkin.el
@@ -7,5 +7,17 @@
 
 ;;; Code:
 
+(defun catkin-source-command (ws command)
+  (format "source %s/devel/setup.bash && %s" ws command))
+
+(defun catkin-print-config (ws)
+  (switch-to-buffer-other-window "*catkin-config*")
+  (erase-buffer)
+  (shell-cd ws)
+  (catkin-source-setup ws)
+  (call-process-shell-command (catkin-source-command ws "catkin config") nil t)
+  (other-window))
+
+(catkin-print-config "~/ros/util")
 
 ;;; catkin.el ends here

3a9b7ee5fa4ae51919fe9ab8d0fb970a6db158b4
goll_th <thore.goll@franka.de>
2019-03-07 10:02:48 +0100

    initial commit

---
+++ catkin.el
@@ -0,0 +1,11 @@
+;;; catkin.el --- Package for compile ROS workspaces with catkin-tools  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  Thore Goll
+
+;; Author:  gollth
+;; Keywords: tools, ROS
+
+;;; Code:
+
+
+;;; catkin.el ends here

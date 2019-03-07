a76b1e91ef0ac56d2834a0a5c4c52fde5f4e20d9
goll_th <thore.goll@franka.de>
2019-03-07 10:10:50 +0100

    add path check and add docstrings

--- catkin.el
+++ catkin.el
@@ -8,9 +8,15 @@
 ;;; Code:
 
 (defun catkin-source-command (ws command)
-  (format "source %s/devel/setup.bash && %s" ws command))
+  "Prepends a source `ws'/devel/setup.bash before `command', if such a file exists"
+  (let ((setup-file (format "%s/devel/setup.bash" ws)))
+    (if (file-exists-p setup-file)
+        (format "source %s && %s" setup-file command)
+      command
+      )))
 
 (defun catkin-print-config (ws)
+  "Prints the catkin config of `ws' to a new buffer called *catkin-config*"
   (switch-to-buffer-other-window "*catkin-config*")
   (erase-buffer)
   (shell-cd ws)
